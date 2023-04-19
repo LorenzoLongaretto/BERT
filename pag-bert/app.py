9a036d6cb63e9f69f06ca601787431098b05c663
LorenzoLongaretto <lorenzolongarettolok@gmail.com>
2023-04-19 15:52:33 -0300

    Update app.py
    correcciones

--- pag-bert/app.py
+++ pag-bert/app.py
@@ -1,9 +1,9 @@
 import torch
 from transformers import AutoTokenizer, AutoModelForQuestionAnswering,pipeline
-from flask import Flask, render_template, request
+#from flask import Flask, render_template, request
 
-model_dir="./modelo"
-tokenizer_dir="./tokenizer"
+model_dir="C:/Users/Loren/Desktop/githubcosas/BERT/BERT/pag-bert/modelo"
+tokenizer_dir="C:/Users/Loren/Desktop/githubcosas/BERT/BERT/pag-bert/tokenizer"
 model = AutoModelForQuestionAnswering.from_pretrained(model_dir)
 tokenizer = AutoTokenizer.from_pretrained(tokenizer_dir)
 
@@ -17,13 +17,13 @@ tokenizer = AutoTokenizer.from_pretrained(tokenizer_dir)
 #     answer = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(encoded_input['input_ids'][0][answer_start:answer_end]))
 #     return answer
 
-def predict(context_input, question_input):
-    encoded_input = tokenizer(question_input, context_input, return_tensors='pt')
-    output = model(**encoded_input)
-    answer_start = torch.argmax(output.start_logits)
-    answer_end = torch.argmax(output.end_logits) + 1
-    answer = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(encoded_input['input_ids'][0][answer_start:answer_end]))
-    return answer
+# def predict(context_input, question_input):
+#     encoded_input = tokenizer(question_input, context_input, return_tensors='pt')
+#     output = model(**encoded_input)
+#     answer_start = torch.argmax(output.start_logits)
+#     answer_end = torch.argmax(output.end_logits) + 1
+#     answer = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(encoded_input['input_ids'][0][answer_start:answer_end]))
+#     return answer
 
 
 from flask import Flask, request, render_template
@@ -36,7 +36,7 @@ def index():
         # obtener el contexto y la pregunta del formulario
         context = request.form['context_input']
         question = request.form['question_input']
-        
+        print(context)
         # cargar el modelo BERT
         salida = pipeline('question-answering', model=model, tokenizer=tokenizer)
         
@@ -44,7 +44,7 @@ def index():
         answer = salida({'context': context, 'question': question})['answer']
         #print(answer)
         # devolver la respuesta a la plantilla HTML
-        return render_template('index.html', answer=answer)
+        return render_template('index.html', answer=answer, context=context)
     else:
         # mostrar la plantilla HTML sin respuesta
         return render_template('index.html')
